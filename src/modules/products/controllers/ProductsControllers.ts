6cde1ab0e37f178a5c5ab3a7cecbbbb6e24901fd
Israel-ops-cmd <israellipe2020@gmail.com>
2025-06-27 10:14:08 -0300

    feat: controller product

---
+++ src/modules/products/controllers/ProductsControllers.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express'
+import ListProductService from '../services/ListProductService'
+import ShowProductService from '../services/ShowProductService'
+import CreateProductService from '../services/CreateProductService'
+import DeleteProductService from '../services/DeleteProductService'
+
+export default class ProductControllers {
+  async index(request: Request, response: Response): Promise<Response> {
+    const listProductsService = new ListProductService()
+    const products = await listProductsService.execute()
+    return response.json(products)
+  }
+
+  async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params
+    const showProductService = new ShowProductService
+    const product = await showProductService.execute({ id })
+    return response.json(product)
+  }
+
+  async create(request: Request, response: Response): Promise<Response> {
+    const { name, price, quantity } = request.body
+    const createProductService = new CreateProductService
+    const product = await createProductService.execute({ name, price, quantity })
+    return response.json(product)
+  }
+
+  async delete(request: Request, response: Response): Promise<Response> {
+    const{ id } = request.params
+    const deleteProductService = new DeleteProductService
+    await deleteProductService.execute({ id })
+    return response.status(204).send([])
+  }
+}
