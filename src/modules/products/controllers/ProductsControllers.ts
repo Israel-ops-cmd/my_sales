6b1ac09c44f91cc08a26994266d120c0afb9fe23
Israel-ops-cmd <israellipe2020@gmail.com>
2025-07-01 08:54:26 -0300

    feat: routes

--- src/modules/products/controllers/ProductsControllers.ts
+++ src/modules/products/controllers/ProductsControllers.ts
@@ -3,6 +3,7 @@ import ListProductService from '../services/ListProductService'
 import ShowProductService from '../services/ShowProductService'
 import CreateProductService from '../services/CreateProductService'
 import DeleteProductService from '../services/DeleteProductService'
+import UpdateProductService from '../services/UpdateProductService'
 
 export default class ProductControllers {
   async index(request: Request, response: Response): Promise<Response> {
@@ -13,21 +14,28 @@ export default class ProductControllers {
 
   async show(request: Request, response: Response): Promise<Response> {
     const { id } = request.params
-    const showProductService = new ShowProductService
+    const showProductService = new ShowProductService()
     const product = await showProductService.execute({ id })
     return response.json(product)
   }
 
   async create(request: Request, response: Response): Promise<Response> {
     const { name, price, quantity } = request.body
-    const createProductService = new CreateProductService
+    const createProductService = new CreateProductService()
     const product = await createProductService.execute({ name, price, quantity })
+    return response.json(product)
+  }
+
+  async update(request: Request, response: Response): Promise<Response> {
+    const { id, name, price, quantity } = request.body
+    const updateProductService = new UpdateProductService()
+    const product = await updateProductService.execute({ id, name, price, quantity })
     return response.json(product)
   }
 
   async delete(request: Request, response: Response): Promise<Response> {
     const{ id } = request.params
-    const deleteProductService = new DeleteProductService
+    const deleteProductService = new DeleteProductService()
     await deleteProductService.execute({ id })
     return response.status(204).send([])
   }
