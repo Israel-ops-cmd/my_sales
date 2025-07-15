810af8f28499c5f2a4cd7a00a2254340df140f7b
Israel-ops-cmd <israellipe2020@gmail.com>
2025-07-15 16:12:45 -0300

    feat: services and controllers customers

---
+++ src/modules/customers/services/DeleteCustomerService.ts
@@ -0,0 +1,18 @@
+import AppError from "shared/errors/AppError"
+import { Customer } from "../database/entities/Customer"
+import { customerRepository } from "../database/repositories/CustomerRepositories"
+
+interface IDeleteCustomer {
+  id: number
+}
+
+export default class DeleteCustomerService {
+  public async execute({ id }: IDeleteCustomer): Promise<Customer> {
+    const customer = await customerRepository.findById(id)
+    if(!customer) {
+      throw new AppError('Customer not found.', 404)
+    }
+
+    await customerRepository.remove(customer)
+  }
+}
